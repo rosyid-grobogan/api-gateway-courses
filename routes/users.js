2ce328673876551dfded0c2cda509f6e8e9dff04
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-28 18:38:40 +0700

    Tested OK

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-var { APP_NAME } = process.env;
+const express = require('express');
+const router = express.Router();
+const { APP_NAME } = process.env;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
