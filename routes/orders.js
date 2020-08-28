49c3472ff6e480b29e946c991f964a0089450c53
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-28 22:35:45 +0700

    register:fixing routers

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('orders');
+  res.send('orders');
 });
 
 module.exports = router;
--- routes/payments.js
+++ routes/payments.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('payments');
+  res.send('payments');
 });
 
 module.exports = router;
