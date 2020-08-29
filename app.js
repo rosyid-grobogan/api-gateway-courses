c39568ac4db21ca01fd9706b4a143ef204476ba9
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-29 20:57:37 +0700

    Integrasi api gateway service-media

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const paymentsRouter = require('./routes/payments');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mediaHandler = require('./handler/media');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send('media');
 });
+
+router.post('/', mediaHandler.create);
 
 module.exports = router;
