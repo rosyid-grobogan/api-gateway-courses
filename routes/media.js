dd20ec6f01583e620b489635aa4c5d268b49e562
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-30 07:07:19 +0700

    Integration: Service Media - POST

--- routes/media.js
+++ routes/media.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mediaHandler = require('./handler/media');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('media');
-});
-
+/* POST */
 router.post('/', mediaHandler.create);
 
 module.exports = router;
