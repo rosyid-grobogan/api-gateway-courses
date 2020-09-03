3ce0733caa8d7040266dabe02f96674ffa4fd254
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-03 17:46:54 +0700

    EndPoint: refresh token

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const coursesRouter = require('./routes/courses');
 const mediaRouter = require('./routes/media');
 const ordersRouter = require('./routes/orders');
 const paymentsRouter = require('./routes/payments');
+const refreshTokensRouter = require('./routes/refreshTokens');
 
 const verifyToken = require('./middlewares/verifyToken');
 
@@ -27,5 +28,6 @@ app.use('/courses', verifyToken, coursesRouter);
 app.use('/media', mediaRouter);
 app.use('/orders', ordersRouter);
 app.use('/payments', paymentsRouter);
+app.use('/refresh-tokens', refreshTokensRouter);
 
 module.exports = app;
