603372d48c289ddcaa7e32707321b04023259889
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-03 21:17:53 +0700

    EndPoint:getUser

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ const verifyToken = require('../middlewares/verifyToken');
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
 router.put('/', verifyToken, userHandler.update);
+router.get('/', verifyToken, userHandler.getUser);
 
 module.exports = router;
