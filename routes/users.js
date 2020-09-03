f02e81bff903793bd4478741cdac6ed5c84e0031
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-03 20:52:51 +0700

    EndPoint:users update

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userHandler = require('./handler/users');
+const verifyToken = require('../middlewares/verifyToken');
 
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
+router.put('/', verifyToken, userHandler.update);
 
 module.exports = router;
