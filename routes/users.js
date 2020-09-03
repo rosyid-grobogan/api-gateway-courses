b4495ab858d60e04ba99243b40e9661d7af3d0c7
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-03 21:37:22 +0700

    EndPoint:logout

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,6 @@ router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
 router.put('/', verifyToken, userHandler.update);
 router.get('/', verifyToken, userHandler.getUser);
+router.post('/logout', verifyToken, userHandler.logout);
 
 module.exports = router;
