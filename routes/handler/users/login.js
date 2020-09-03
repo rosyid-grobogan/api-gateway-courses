a61bdb1358e0592664c5d4c5002906bbb14682e0
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-03 07:31:02 +0700

    Service User:login |token & refresh token

--- routes/handler/users/login.js
+++ routes/handler/users/login.js
@@ -13,8 +13,26 @@ const api = apiAdapter(URL_SERVICE_USER);
 module.exports = async (req, res) => {
   try {
     const user = await api.post('/users/login', req.body);
+    const data = user.data.data;
+    const token = jwt.sign({ data }, JWT_SECRET, {
+      expiresIn: JWT_ACCESS_TOKEN_EXPIRED,
+    });
+    const refreshToken = jwt.sign({ data }, JWT_SECRET_REFRESH_TOKEN, {
+      expiresIn: JWT_REFRESH_TOKEN_EXPIRED,
+    });
 
-    return res.json(user.data);
+    await api.post('/refresh-tokens', {
+      refresh_token: refreshToken,
+      user_id: data.id,
+    });
+
+    return res.json({
+      status: 'success',
+      data: {
+        token,
+        refresh_token: refreshToken,
+      },
+    });
   } catch (error) {
     if (error.code === 'ECONNREFUSED') {
       return res.status(500).json({
